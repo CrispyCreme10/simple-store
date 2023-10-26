b6b7bfb69472fd6fc7ea426019695d07bb269040
Jordan R <jordanrenner69@gmail.com>
2023-10-26 18:22:44 -0500

    init

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderNavComponent } from './customer/shared/components/header-nav/header-nav.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderNavComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/src/app/customer/shared/components/header-nav/header-nav.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header-nav',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header-nav.component.html',
+  styleUrls: ['./header-nav.component.scss']
+})
+export class HeaderNavComponent {
+
+}
